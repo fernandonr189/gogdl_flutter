2305de7ad969a8c5fc279b901e5bf3259a6fcece
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2025-10-05 16:10:34 -0600

    Added builds param to GogDbGameDetails

--- rust/src/api/games_downloader.rs
+++ rust/src/api/games_downloader.rs
@@ -27,7 +27,7 @@ impl GamesDownloader {
             "https://www.gogdb.org/data/products/{}/product.json",
             game_id
         );
-        let resp: GogDbGameDetails = self.session.get_request(url).await?;
+        let resp: GogDbGameDetails = self.session.get_request_unauthorized(url).await?;
         Ok(resp)
     }
 }
@@ -38,6 +38,7 @@ pub struct GogDbGameDetails {
     pub image_boxart: Option<String>,
     #[serde(rename = "type")]
     pub product_type: Option<String>,
+    pub builds: Vec<GogDbGameBuild>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -45,92 +46,9 @@ pub struct OwnedGamesResponse {
     pub owned: Vec<u64>,
 }
 
-#[derive(Debug, Deserialize, Clone, Default)]
-#[serde(default)]
-pub struct GameDetailsResponse {
-    pub title: Option<String>,
-    #[serde(rename = "backgroundImage")]
-    pub background_image: Option<String>,
-    pub cd_key: Option<String>,
-    pub text_information: Option<String>,
-    pub downloads: Vec<(String, DownloadsByLanguage)>,
-    #[serde(rename = "galaxyDownloads")]
-    pub galaxy_downloads: Vec<DownloadPlatform>,
-    pub extras: Vec<ExtraFile>,
-    pub dlcs: Vec<Dlc>,
-    pub tags: Vec<String>,
-    pub is_preorder: Option<bool>,
-    pub release_timestamp: Option<i64>,
-    pub messages: Vec<String>,
-    pub changelog: Option<String>,
-    pub forum_link: Option<String>,
-    pub is_base_product_missing: Option<bool>,
-    pub missing_base_product: Option<String>,
-    pub features: Vec<String>,
-    #[serde(rename = "simpleGalaxyInstallers")]
-    pub simple_galaxy_installers: Vec<SimpleGalaxyInstaller>,
-}
-
-#[derive(Debug, Deserialize, Clone, Default)]
-#[serde(default)]
-pub struct DownloadsByLanguage {
-    pub windows: Vec<DownloadFile>,
-    pub mac: Vec<DownloadFile>,
-    pub linux: Vec<DownloadFile>,
-}
-
-#[derive(Debug, Deserialize, Clone, Default)]
-#[serde(default)]
-pub struct DownloadFile {
-    #[serde(rename = "manualUrl")]
-    pub manual_url: Option<String>,
-    #[serde(rename = "downloaderUrl")]
-    pub downloader_url: Option<String>,
-    pub name: Option<String>,
-    pub version: Option<String>,
-    pub date: Option<String>,
-    pub size: Option<String>,
-    pub os: Option<String>, // optional platform info if present
-}
-
-#[derive(Debug, Deserialize, Clone, Default)]
-#[serde(default)]
-pub struct ExtraFile {
-    #[serde(rename = "manualUrl")]
-    pub manual_url: Option<String>,
-    #[serde(rename = "downloaderUrl")]
-    pub downloader_url: Option<String>,
-    pub name: Option<String>,
-    #[serde(rename = "type")]
-    pub file_type: Option<String>,
-    pub info: Option<u32>,
-    pub size: Option<String>,
-}
-
-#[derive(Debug, Deserialize, Clone, Default)]
-#[serde(default)]
-pub struct Dlc {
-    pub id: Option<u64>,
-    pub title: Option<String>,
-}
-
-#[derive(Debug, Deserialize, Clone, Default)]
-#[serde(default)]
-pub struct SimpleGalaxyInstaller {
-    pub path: Option<String>,
-    pub os: Option<String>,
-}
-
-#[derive(Debug, Deserialize, Clone, Default)]
-#[serde(default)]
-pub struct DownloadPlatform {
-    #[serde(rename = "manualUrl")]
-    pub manual_url: Option<String>,
-    #[serde(rename = "downloaderUrl")]
-    pub downloader_url: Option<String>,
-    pub name: Option<String>,
-    pub version: Option<String>,
-    pub date: Option<String>,
-    pub size: Option<String>,
-    pub os: Option<String>,
+#[derive(Deserialize, Debug)]
+pub struct GogDbGameBuild {
+    pub date_published: Option<String>,
+    pub generation: Option<i32>,
+    pub link: Option<String>,
 }
